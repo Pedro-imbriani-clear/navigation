7cb9ebc79a0d731c1abfeb4ee6afc3f6807d82b4
pedro-imbriani-clear <pedro.imbriani.clear@gmail.com>
2023-07-31 22:10:50 -0300

    working with icons

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Feather from 'react-native-vector-icons/Feather'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,7 +7,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FontAwesome
+        name='user'
+        size={35}
+        color='#54a300'
+      />
+      <FontAwesome
+        name='home'
+        size={35}
+        color='#11118c'
+      />
+      <Feather
+      name='gift'
+      size={35}
+      color='#7665ff'
+      />
+
     </View>
   );
 }
